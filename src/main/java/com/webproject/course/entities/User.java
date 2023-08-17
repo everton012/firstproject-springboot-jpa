6dfdd40f256c625c8b347eba741da1b10df5091c
Everton Clemente <everton.cl.souza@hotmail.com>
2023-08-17 20:04:02 -0300

    H2 database, test profile, JPA

--- src/main/java/com/webproject/course/entities/User.java
+++ src/main/java/com/webproject/course/entities/User.java
@@ -3,12 +3,19 @@ package com.webproject.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@EntityScan
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
