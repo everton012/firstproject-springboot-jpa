d1e09a05f71bbdebef63c2f3db2274003820c957
Everton Clemente <everton.cl.souza@hotmail.com>
2023-08-18 22:09:19 -0300

    Order, instant, Iso 8601

---
+++ src/main/java/com/webproject/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.webproject.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webproject.course.entities.Order;
+import com.webproject.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+	}
+
+}
